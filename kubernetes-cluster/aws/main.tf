ae49dae500f8063a5456a7de8d67221e9f2feb9d
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-01-15 12:39:24 -0800

    feat: pin aws k8s module

--- kubernetes-cluster/aws/main.tf
+++ kubernetes-cluster/aws/main.tf
@@ -1,7 +1,7 @@
 
 variable "kubernetes_cluster_configurations" {}
 module "captain" {
-  source         = "git::https://github.com/GlueOps/terraform-module-cloud-aws-kubernetes-cluster"
+  source         = "git::https://github.com/GlueOps/terraform-module-cloud-aws-kubernetes-cluster.git?ref=v0.1.0"
   vpc_cidr_block = var.kubernetes_cluster_configurations.vpc_cidr_block
   region         = var.kubernetes_cluster_configurations.region
   eks_node_group = var.kubernetes_cluster_configurations.eks_node_group
