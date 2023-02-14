db49517a4c948d867925911f7005f2a57a19c977
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-02-14 13:16:04 -0800

    fix: updating gcp module from v0.2.1 -> v0.2.2 so that we are no longer using spot instances when zonal is false (#23)

--- kubernetes-cluster/gcp/main.tf
+++ kubernetes-cluster/gcp/main.tf
@@ -1,7 +1,7 @@
 
 variable "kubernetes_cluster_configurations" {}
 module "captain" {
-  source         = "git::https://github.com/GlueOps/terraform-module-cloud-gcp-kubernetes-cluster.git?ref=v0.2.1"
+  source         = "git::https://github.com/GlueOps/terraform-module-cloud-gcp-kubernetes-cluster.git?ref=v0.2.2"
   network_ranges = var.kubernetes_cluster_configurations.network_ranges
   project_id     = var.kubernetes_cluster_configurations.project_id
   region         = var.kubernetes_cluster_configurations.region
