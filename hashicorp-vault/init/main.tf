ee73e852bcf610fef731070e46957daa0d5186ea
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-02-17 21:41:09 -0800

    Feat/updates (#25)
    * feat: updating gke cluster to 1.24.X
    * feat: updating aws cluster to 1.24.X
    * feat: updating platform to v0.19.0
    * feat: updating vault config from v0.2.0 -> v0.2.1
    * feat: update vault init from v0.1.0 -> v0.1.1
    
    ---------
    
    Co-authored-by: fernandoataoldotcom <antonio.rodriguez@glueops.dev>

--- hashicorp-vault/init/main.tf
+++ hashicorp-vault/init/main.tf
@@ -1,3 +1,3 @@
 module "initialize_vault_cluster" {
-  source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-initialization.git?ref=v0.1.0"
+  source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-initialization.git?ref=v0.1.1"
 }
