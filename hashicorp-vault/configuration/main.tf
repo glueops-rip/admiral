ab39144fd7587e6a9853d891d9e1def0501eef7a
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-23 10:42:30 -0700

    Feat/updates (#33)
    * update vault modules
    
    * feat: update glueops-platform to v0.24.0
    
    * feat: update gke version

--- hashicorp-vault/configuration/main.tf
+++ hashicorp-vault/configuration/main.tf
@@ -1,6 +1,6 @@
 variable "vault_configurations" {}
 module "initialize_vault_cluster" {
-  source                  = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git?ref=v0.2.1"
+  source                  = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git?ref=v0.3.1"
   backends                = var.vault_configurations.backends
   org_team_policy_mapping = var.vault_configurations.org_team_policy_mapping
 }
--- hashicorp-vault/init/main.tf
+++ hashicorp-vault/init/main.tf
@@ -1,3 +1,3 @@
 module "initialize_vault_cluster" {
-  source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-initialization.git?ref=v0.1.1"
+  source = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-initialization.git?ref=v0.2.0"
 }
