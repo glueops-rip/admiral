0636340ec533aca6d8638ab36f21d23afb9bde31
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-04-05 05:01:23 +0000

    MAJOR: update vault-configuration to use OIDC and to use new group convention

--- hashicorp-vault/configuration/main.tf
+++ hashicorp-vault/configuration/main.tf
@@ -1,6 +1,7 @@
 variable "vault_configurations" {}
 module "initialize_vault_cluster" {
-  source                  = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git?ref=v0.3.1"
-  backends                = var.vault_configurations.backends
-  org_team_policy_mapping = var.vault_configurations.org_team_policy_mapping
+  source                   = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git?ref=v0.4.0"
+  captain_domain           = var.vault_configurations.captain_domain
+  oidc_client_secret       = var.vault_configurations.oidc_client_secret
+  org_team_policy_mappings = var.vault_configurations.org_team_policy_mappings
 }
