f2b2adc1ccf8a3c93c161fbd275f41f04b6f573d
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-04-11 14:46:14 -0700

    Delete hashicorp-vault directory

--- hashicorp-vault/configuration/main.tf
+++
@@ -1,7 +0,0 @@
-variable "vault_configurations" {}
-module "initialize_vault_cluster" {
-  source                   = "git::https://github.com/GlueOps/terraform-module-kubernetes-hashicorp-vault-configuration.git?ref=v0.4.0"
-  captain_domain           = var.vault_configurations.captain_domain
-  oidc_client_secret       = var.vault_configurations.oidc_client_secret
-  org_team_policy_mappings = var.vault_configurations.org_team_policy_mappings
-}
