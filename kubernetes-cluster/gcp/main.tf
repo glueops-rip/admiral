0b9a1175f14f498449a1e2e9ad4d9511f1962a61
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-04-11 14:46:23 -0700

    Delete kubernetes-cluster directory

--- kubernetes-cluster/gcp/main.tf
+++
@@ -1,9 +0,0 @@
-
-variable "kubernetes_cluster_configurations" {}
-module "captain" {
-  source         = "git::https://github.com/GlueOps/terraform-module-cloud-gcp-kubernetes-cluster.git?ref=v0.4.0"
-  network_ranges = var.kubernetes_cluster_configurations.network_ranges
-  project_id     = var.kubernetes_cluster_configurations.project_id
-  region         = var.kubernetes_cluster_configurations.region
-  zonal          = var.kubernetes_cluster_configurations.zonal
-}
